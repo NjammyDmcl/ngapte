29f4d9b4b9f47e7ec0267c57ebbaab19c58828ef
njammyDmcl <njammypatchakwejovani@gmail.com>
2020-07-29 03:25:31 +0100

    creat factories and seeder

---
+++ database/factories/visitor.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\s;
+use Faker\Generator as Faker;
+
+$factory->define(s::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
