77bf4f3a3610fb4e21a0d7e0988c8f32d2629a87
njammyDmcl <njammypatchakwejovani@gmail.com>
2020-07-30 06:04:54 +0100

    de

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \Torann\GeoIP\Facades\GeoIP;
-
+use Filipac\Ip;
 class HomeController extends Controller {
 //composer require geoip2/geoip2
 
     function index(Request $request) {
-        $ip = $request->ip();
+        //$ip = $request->ip();
         //dd($ip);
-        $loc = geoip()->getLocation($ip);
+        $loc = geoip()->getLocation(Ip::get());
         dd($loc);
         return view('home');
     }
